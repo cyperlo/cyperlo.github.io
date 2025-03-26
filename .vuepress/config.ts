2303fa683ec60eea42647d5a750f4a4654c80817
hailong <17628689483@163.com>
2025-03-26 11:32:23 +0800

    feat: config

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -10,6 +10,7 @@ const tags = ["程序员", "编程", "计算机"];
 export default defineConfig({
   title: "cyperlo",
   description: "贴心的编程学习路线，全面的编程知识百科",
+  base: "/" as `/${string}/`,
   head: [
     // 站点图标
     ["link", { rel: "icon", href: "/favicon.ico" }],
